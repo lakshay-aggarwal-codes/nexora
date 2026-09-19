18203bf8f439bc4cd18f037c9997881a632c73dd
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-09-19 11:13:02 +0530

    add gatewau and authentication struture

---
+++ backend/gateway/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import proxy from "express-http-proxy";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/auth", proxy(process.env.AUTH_SERVICE));
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello from gateway" });
+});
+
+app.listen(port, () => {
+  console.log(`gateway started at ${port}`);
+});
