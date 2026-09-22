7a15d97eeced59242cc666e51c24f3eb5ab71ebc
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-09-22 10:28:14 +0530

    create chat agent and agent routes

--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";  
+import router from "./routes/agent.routes.js";
 dotenv.config();
 
 const port = process.env.PORT || 8001;
 
 const app = express();
 app.use(express.json());  
+app.use('/', router)
 app.get("/", (req, res) => {
   res.json({ message: "hello from agent" });
 });
---
+++ backend/services/agent/routes/agent.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { agent } from "../controller/agent.controller.js";
+
+const router = express.Router();
+
+router.post('/chat',agent)
+export default router
